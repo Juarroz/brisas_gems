e9f191f038f4347a932b3d0dd621748c11135291
Natalia <natalia.qek@gmail.com>
2025-09-25 17:11:20 -0500

    Reestructuracion del proyecto

---
+++ legacy/php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "brisas_gems"; 
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
